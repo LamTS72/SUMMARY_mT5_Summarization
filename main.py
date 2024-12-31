04874184314bd52a10df76d74123283c1b5d9082
Chessmen <sonlam7220@gmail.com>
2024-12-31 21:18:15 +0700

    add readme

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ from transformers import pipeline
 st.title('Langchain Demo With Summarization Of Text')
 input_text=st.text_input("Input text")
 
-model_path = "Chessmen/summary"
+model_path = "huggingface-course/mt5-small-finetuned-amazon-en-es"
 summary_task  = pipeline(
     "summarization",
     model=model_path,
 )
 
 if input_text:
-	st.write(summary_task(input_text))
+	st.write(summary_task(input_text, max_new_tokens=32))
     
